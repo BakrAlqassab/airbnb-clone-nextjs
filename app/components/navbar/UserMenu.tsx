aa0945b9cf7063e7b6563645aeb4099e57bcc359
bakr <alqassab31@gmail.com>
2024-03-19 11:14:10 +0200

    small fixes in eslint and Mdoal

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -18,30 +18,22 @@ export default function UserMenu() {
                 <div onClick={() => {
                     console.log("weeee")
                 }}
-
                      className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer">Airbnb
                     your home
                 </div>
-                <div onClick={toggleOpen}  className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-100 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"  >
+                <div onClick={toggleOpen}  className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-100 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition">
                     <AiOutlineMenu/>
-                    <div className="hodden md:block">
+                    <div className="hidden md:block">
                         <Avatar/>
                     </div>
                 </div>
             </div>
             {isOpen &&  (
-
-
                 <div className="absolute rounded-xl shadow-md w-[40vw] ms:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
                     <div className="flex flex-col cursor-pointer">
                         <>
                         <MenuItem onClick={()=> (console.log("login"))}    label={"Login"}/>
                         <MenuItem onClick={()=> (console.log("Sign up"))}    label={"Sign up"}/>
-
-
-
-
-
 
                         </>
                     </div>
