f2fb23b9e69e5fa76ccd7650ba754f93c71f09d9
unknown <alqassab31@gmail.com>
2024-06-13 15:23:53 +0300

    add Docker Image

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,6 +1,6 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Nunito } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
 import Navbar from "@/app/components/navbar/Navbar";
 import RegisterModal from "@/app/components/modals/RegisterModal";
 import LoginModal from "@/app/components/modals/LoginModal";
@@ -8,38 +8,33 @@ import SearchModal from "@/app/components/modals/SearchModal";
 import ToasterProvider from "@/app/providers/ToasterProvider";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import RentModal from "@/app/components/modals/RentModal";
- import {useLocale} from "next-intl";
+import { useLocale } from "next-intl";
 
-const font = Nunito({ subsets: ['latin'] })
-
-
-
+const font = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Airbnb App',
-  description: 'Airbnb App clone project',
-}
+  title: "Airbnb App",
+  description: "Airbnb App clone project",
+};
 
-export default async function RootLayout({
-  children
+export default function RootLayout({
+  children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-    const currentUser = await getCurrentUser()
-     const lang = useLocale();
+  const currentUser = getCurrentUser();
+  const lang = useLocale();
   return (
-      <html lang={lang}>
-      <body className={font.className} >
-      <ToasterProvider/>
-      <RegisterModal/>
-      <LoginModal/>
-      <SearchModal/>
-      <RentModal/>
-      <Navbar currentUser={currentUser}/>
-      <div className="pb-20 pt-28 h-full z-40">
-        {children}
-      </div>
+    <html lang={lang}>
+      <body className={font.className}>
+        <ToasterProvider />
+        <RegisterModal />
+        <LoginModal />
+        <SearchModal />
+        <RentModal />
+        <Navbar currentUser={currentUser} />
+        <div className="pb-20 pt-28 h-full z-40">{children}</div>
       </body>
-      </html>
-  )
+    </html>
+  );
 }
