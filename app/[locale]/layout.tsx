79f158123a9b6a49610ddfe949dfa647d44bc557
bakr <alqassab31@gmail.com>
2024-09-06 17:54:44 +0300

    check which file is readed

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,5 +1,5 @@
 import "./styles/globals.css";
-// import "./styles/tailwind.css";
+import "./styles/tailwind.css";
 
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
