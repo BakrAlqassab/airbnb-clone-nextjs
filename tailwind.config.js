074f505e17ee7658bf1b4e8a878bdaf374adfc94
bakr <alqassab31@gmail.com>
2024-09-06 15:28:44 +0300

    more trying

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,30 @@
+// module.exports = {
+//   content: [
+//     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./app/[locale]/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+//   important: true,
+//   safelist: [
+//     "bg-red-500",
+//     "text-center",
+//     "flex",
+//     "w-full",
+//     "absolute",
+//     "relavent",
+//   ],
+// };
+
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/[locale]/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
   important: true,
-  safelist: [
-    "bg-red-500",
-    "text-center",
-    "flex",
-    "w-full",
-    "absolute",
-    "relavent",
-  ],
 };
